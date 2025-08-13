4815a9e3d60c666befb238d4bc3f1c405e96390c
nestorivan <nlopezdev@gmail.com>
2025-08-13 10:23:05 -0700

    implement zod validation globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from "@nestjs/common";
 import { CharactersModule } from "./modules/characters/characters.module";
 import { JobsModule } from "./modules/jobs/jobs.module";
 import { BattleModule } from "./modules/battle/battle.module";
+import { APP_PIPE } from "@nestjs/core";
+import { ZodValidationPipe } from "nestjs-zod";
 
 @Module({
   imports: [CharactersModule, JobsModule, BattleModule],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
