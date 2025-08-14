83d868543ccecceb4dfb4fc6a235bcf718514d43
nestorivan <nlopezdev@gmail.com>
2025-08-13 23:11:29 -0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { VersioningType } from "@nestjs/common";
+import { patchNestJsSwagger } from "nestjs-zod";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +11,16 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: "1",
   });
+
+  const config = new DocumentBuilder()
+    .setTitle("Neo RPG API")
+    .setDescription("API for Neo RPG")
+    .setVersion("1.0")
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  patchNestJsSwagger();
+  SwaggerModule.setup("api", app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
