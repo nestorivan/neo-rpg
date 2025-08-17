53e96e381a6936960c75c7edc1df54f3cce77dbe
nestorivan <nlopezdev@gmail.com>
2025-08-16 21:49:50 -0700

    add swagger documentation

--- src/modules/battle/controllers/battle.controller.ts
+++ src/modules/battle/controllers/battle.controller.ts
@@ -2,12 +2,19 @@ import { Body, Controller, Post } from "@nestjs/common";
 import { BattleService } from "../services/battle.service";
 import { CreateBattleDto } from "../dto/battle.dto";
 import { LogMethod } from "@src/common/decorators/logMethod.decorator";
+import { ApiBody, ApiOperation } from "@nestjs/swagger";
 
 @Controller("battle")
 export class BattleController {
   constructor(private readonly battleService: BattleService) {}
 
   @Post()
+  @ApiOperation({
+    summary: "Create a new battle",
+    description:
+      "Starts a new battle between two characters based on the provided details. Requires both character IDs in the request body.",
+  })
+  @ApiBody({ type: CreateBattleDto })
   @LogMethod()
   create(@Body() createBattleDto: CreateBattleDto) {
     return this.battleService.create(createBattleDto);
--- src/modules/characters/controllers/characters.controller.ts
+++ src/modules/characters/controllers/characters.controller.ts
@@ -10,22 +10,35 @@ import {
 import {
   CreateCharacterDto,
   CharacterByIdSchema,
+  CharacterDto,
 } from "../dto/createCharacter.dto";
 import { ZodValidationPipe } from "nestjs-zod";
 import { CharactersService } from "../services/characters.service";
 import { LogMethod } from "@src/common/decorators/logMethod.decorator";
+import { ApiBody, ApiOperation, ApiQuery } from "@nestjs/swagger";
 
 @Controller("characters")
 export class CharactersController {
   constructor(private readonly charactersService: CharactersService) {}
 
   @Get()
+  @ApiOperation({
+    summary: "Retrieve all characters",
+    description:
+      "Fetches and returns a list of all available characters in the system.",
+  })
   @LogMethod()
   findAll() {
     return this.charactersService.findAll();
   }
 
   @Get(":id")
+  @ApiOperation({
+    summary: "Get a character by ID",
+    description:
+      "Retrieves a single character by its unique ID. Returns character details if found.",
+  })
+  @ApiQuery({ name: "id", required: true, type: Number })
   @LogMethod()
   findOne(
     @Param("id", ParseIntPipe, new ZodValidationPipe(CharacterByIdSchema))
@@ -35,17 +48,26 @@ export class CharactersController {
   }
 
   @Post()
+  @ApiOperation({
+    summary: "Create a new character",
+    description:
+      "Creates a new character with the provided details. Requires a name and job type in the request body.",
+  })
+  @ApiBody({ type: CreateCharacterDto })
   @LogMethod()
   create(@Body() createCharacterDto: CreateCharacterDto) {
     return this.charactersService.create(createCharacterDto);
   }
 
   @Patch(":id")
+  @ApiOperation({
+    summary: "Update an existing character",
+    description:
+      "Updates the details of an existing character based on the provided ID. Allows updating fields such as name and job type.",
+  })
+  @ApiBody({ type: CharacterDto })
   @LogMethod()
-  update(
-    @Param("id") id: number,
-    @Body() updateCharacterDto: CreateCharacterDto
-  ) {
+  update(@Param("id") id: number, @Body() updateCharacterDto: CharacterDto) {
     return this.charactersService.update(id, updateCharacterDto);
   }
 }
